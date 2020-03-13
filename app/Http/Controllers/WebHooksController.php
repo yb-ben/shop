6e7ef74a0c53c090557a9a3d4f775dfbba0f7e15
804141892@qq.com <804141892@qq.com>
2020-03-13 10:23:13 +0800

    save

--- app/Http/Controllers/WebHooksController.php
+++ app/Http/Controllers/WebHooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebHooksController extends Controller{
 
@@ -10,8 +11,11 @@ class WebHooksController extends Controller{
 
 
     public function onPush(Request $request){
+        $target = app_path();
         $data = $request->post();
-        print_r($data);
+        exec('git pull',$output,$ret);
+        Log::info('github event',$data);
+        Log::info("cd $target && exec git pull",['output' => $output,'ret' => $ret]);
         return 'ok';
     }
 }
