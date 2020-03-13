2c3bb7e94585da8f34f08dbbe27eced3c3f0501b
804141892@qq.com <804141892@qq.com>
2020-03-13 10:42:52 +0800

    save

--- app/Http/Controllers/WebHooksController.php
+++ app/Http/Controllers/WebHooksController.php
@@ -13,8 +13,13 @@ class WebHooksController extends Controller{
     public function onPush(Request $request){
         $target = base_path();
         $data = $request->post();
+        $token = env('WEBHOOkS_TOKEN');
+
+        if(!isset($data['token']) || $data['token'] !== $token ){
+            Log::info('webhooks token not equals',['value' => $data['token'],'expect' => $token]);
+            return 'false';
+        }
         exec('git pull',$output,$ret);
-        Log::info('github event',$data);
         Log::info("cd $target && exec git pull",['output' => $output,'ret' => $ret]);
         return 'ok';
     }
