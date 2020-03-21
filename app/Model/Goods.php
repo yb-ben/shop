820bd64b839d0552bce53c029b3a6b8b011c7f8d
804141892@qq.com <804141892@qq.com>
2020-03-21 15:38:21 +0800

    save

--- app/Model/Goods.php
+++ app/Model/Goods.php
@@ -62,7 +62,7 @@ class Goods extends Base{
     }
 
     public function getSpuAttribute($value){
-        $value = json_decode($value);
+        $value = json_decode($value,JSON_OBJECT_AS_ARRAY);
         foreach($value as &$v){
             if(!empty($v['path'])){
                 $v['path_full'] = env('APP_URL').$v['path'];
