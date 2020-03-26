02fbbb9c34ccab634dc7df28efa6c511f327439f
804141892@qq.com <804141892@qq.com>
2020-03-26 10:27:49 +0800

    save

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,7 +11,11 @@ class TestController extends Controller{
     public function index(){
 
 
-        Test::dispatch()->delay(now()->addMinutes(10));
+        Test::dispatch()
+        ->delay(now()->addMinutes(10))
+        ->onConnection('redis')
+        ->onQueue('test_job')
+        ;
         return 'ok';
     }
 }
