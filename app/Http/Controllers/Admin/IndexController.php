eff3e74d9af23e04d59b4c19762f050f6e9d300a
huyibin <804141892@qq.com>
2020-04-24 17:35:54 +0800

    save

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Logic\Cart\IndexLogic;
 use App\Jobs\Order\ExpireOrder;
+use App\Jobs\Test;
 use App\Model\Payment;
 use App\Utils\Response;
+use Illuminate\Support\Facades\Redis;
 
 class IndexController extends Controller{
 
     public function index(){
 
-        ExpireOrder::dispatch('1')->delay(60)->onQueue('order')->afterResponse();
-
+        dispatch(new ExpireOrder('1'))->delay(60)->afterResponse();
+        dispatch(new Test())->delay(60)->afterResponse();
         return Response::api();
     }
 
