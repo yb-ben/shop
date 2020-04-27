3fc7885cd9268bc22c480b4df028ffde5e4cc90b
huyibin <804141892@qq.com>
2020-04-27 21:42:08 +0800

    save

--- app/Http/Controllers/Payment/AlipayController.php
+++ app/Http/Controllers/Payment/AlipayController.php
@@ -73,11 +73,8 @@ class AlipayController extends Controller
             $data = $request->except(['_url']);
             Log::channel('alipay_notify')->info('Alipay sync '.var_export($data,true));
 
-             Pay::alipay();
-            if(!Support::verifySign($data,true)){
-                Events::dispatch(new Events\SignFailed('Alipay', '', $data));
-                throw new InvalidSignException('Alipay Sign Verify FAILED');
-            }
+             $data = Pay::alipay()->verify($data)->toArray();
+
 
 
             DB::transaction(function ()use($data){
